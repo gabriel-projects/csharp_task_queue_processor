f38b26e1d03cbea204fe153e450f74c5845f4b2c
Gabriel Rossi <gabriel.projetosgit@gmail.com>
2025-03-27 22:07:19 -0300

    feat: setup inicial

---
+++ Api.GRRInnovations.Task.Queue.Processor/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Api.GRRInnovations.Task.Queue.Processor.Application;
+using Api.GRRInnovations.Task.Queue.Processor.Infrastructure;
+
+namespace Api.GRRInnovations.Task.Queue.Processor
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddInfrastructureServices();
+            services.AddApplicationServices();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
